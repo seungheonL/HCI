59330a2643884a18d0c3aa77388b413178c276b2
seungheon <rumah800@naver.com>
2022-03-10 22:07:12 +0900

    Add comment

--- OpenCV_1/main.cpp
+++ OpenCV_1/main.cpp
@@ -9,7 +9,7 @@ int main()
 	CPixel cpixel;
 
 	Mat stuff_color_1 = imread("image/stuff_color_1.png", 1); //0이면 흑백, 1이면 컬러
-	Mat stuff_color_2 = imread("image/stuff_color_2.png", 1);
+	Mat stuff_color_2 = imread("image/stuff_color_2.png", 1); //0이면 흑백, 1이면 컬러
 
 	Mat result = cpixel.GS_add_image(stuff_color_1, stuff_color_2);
 	imshow("Addition with two images", result);
