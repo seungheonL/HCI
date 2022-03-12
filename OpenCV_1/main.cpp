c6fdf467c7bf6e4441282698ff43c9a014b483f5
seungheon <rumah800@naver.com>
2022-03-12 17:44:12 +0900

    Change comment and add gray image

--- OpenCV_1/main.cpp
+++ OpenCV_1/main.cpp
@@ -21,7 +21,7 @@ int main()
 	imshow("Addition with two images", result); // Output 이미지 출력
 	imwrite("result/result1-i.png", result); // Output 이미지를 내 프로젝트에 별도로 저장
 
-	waitKey(); // Output 이미지들을 한 장씩 보기 위해 별도의 입력이 있을 때까지 기다린다.
+	waitKey(); // Output 이미지를 관찰하기 위해 별도의 입력이 있을 때까지 기다린다.
 
 	// 1-ii
 	result = cpixel.GS_add_image(lenna, orange); // lenna와 orange의 대응되는 픽셀값을 더한 결과를 저장
@@ -29,7 +29,7 @@ int main()
 	imshow("Addition with two images", result); // Output 이미지 출력
 	imwrite("result/result1-ii.png", result); // Output 이미지를 내 프로젝트에 별도로 저장
 
-	waitKey(); // Output 이미지들을 한 장씩 보기 위해 별도의 입력이 있을 때까지 기다린다.
+	waitKey(); // Output 이미지를 관찰하기 위해 별도의 입력이 있을 때까지 기다린다.
 
 	// 2-iii
 	result = cpixel.GS_subtract_image(stuff_color_1, stuff_color_2); // stuff_color_1과 stuff_color_2의 대응되는 픽셀값을 뺀 결과를 저장
@@ -37,7 +37,7 @@ int main()
 	imshow("Subtraction with two images", result); // Output 이미지 출력
 	imwrite("result/result2-iii.png", result); // Output 이미지를 내 프로젝트에 별도로 저장
 
-	waitKey(); // Output 이미지들을 한 장씩 보기 위해 별도의 입력이 있을 때까지 기다린다.
+	waitKey(); // Output 이미지를 관찰하기 위해 별도의 입력이 있을 때까지 기다린다.
 
 	// 2-iv
 	result = cpixel.GS_subtract_image(lenna, orange); // lenna와 orange의 대응되는 픽셀값을 뺀 결과를 저장
@@ -45,7 +45,7 @@ int main()
 	imshow("Subtraction with two images", result); // Output 이미지 출력
 	imwrite("result/result2-iv.png", result); // Output 이미지를 내 프로젝트에 별도로 저장
 
-	waitKey(); // Output 이미지들을 한 장씩 보기 위해 별도의 입력이 있을 때까지 기다린다.
+	waitKey(); // Output 이미지를 관찰하기 위해 별도의 입력이 있을 때까지 기다린다.
 
 	// 3-i
 	// stuff_color_1과 stuff_color_2를 적절히 섞는다.
@@ -56,7 +56,7 @@ int main()
 	imshow("Blending with two images", result); // Output 이미지 출력
 	imwrite("result/result3-i.png", result); // Output 이미지를 내 프로젝트에 별도로 저장
 
-	waitKey(); // Output 이미지들을 한 장씩 보기 위해 별도의 입력이 있을 때까지 기다린다.
+	waitKey(); // Output 이미지를 관찰하기 위해 별도의 입력이 있을 때까지 기다린다.
 
 	// 3-ii
 	// lenna와 orange를 적절히 섞는다.
@@ -67,27 +67,31 @@ int main()
 	imshow("Blending with two images", result); // Output 이미지 출력
 	imwrite("result/result3-ii.png", result); // Output 이미지를 내 프로젝트에 별도로 저장
 
-	waitKey(); // Output 이미지들을 한 장씩 보기 위해 별도의 입력이 있을 때까지 기다린다.
+	waitKey(); // Output 이미지를 관찰하기 위해 별도의 입력이 있을 때까지 기다린다.
 
 	// 4-i
 	Mat stuff_color_1_gray; // stuff_color_1의 흑백 이미지를 저장하기 위한 Mat 변수 생성
 	cvtColor(stuff_color_1, stuff_color_1_gray, COLOR_BGR2GRAY); // cvtColor() 함수를 이용하여 gray-scale로 변환해준다.
+	imwrite("result/stuff_color_1_gray.png", stuff_color_1_gray); // stuff_color_1의 흑백 이미지 저장
+
 	result = cpixel.GS_LUT_basic_contrast_brightness(stuff_color_1_gray, 2.4, 5); // contrast와 brightness의 수치를 적절히 결정하여 LUT를 만든 후 이미지를 변환한다.
 
 	imshow("Contrast & brightness adjustment by LUT", result); // Output 이미지 출력
 	imwrite("result/result4-i.png", result); // Output 이미지를 내 프로젝트에 별도로 저장
 
-	waitKey(); // Output 이미지들을 한 장씩 보기 위해 별도의 입력이 있을 때까지 기다린다.
+	waitKey(); // Output 이미지를 관찰하기 위해 별도의 입력이 있을 때까지 기다린다.
 
 	// 4-ii
 	Mat lenna_gray; // lenna의 흑백 이미지를 저장하기 위한 Mat 변수 생성
-	cvtColor(lenna, lenna_gray, COLOR_BGR2GRAY); // cvtColor() 함수를 이용하여 gray-scale로 변환해준다.
+	cvtColor(lenna, lenna_gray, COLOR_BGR2GRAY); // cvtColor() 함수를 이용하여 gray-scale로 변환해준다
+	imwrite("result/lenna_gray.png", lenna_gray); // Output 이미지를 내 프로젝트에 별도로 저장
+
 	result = cpixel.GS_LUT_basic_contrast_brightness(lenna_gray, 1.6, 10); // contrast와 brightness의 수치를 적절히 결정하여 LUT를 만든 후 이미지를 변환한다.
 
 	imshow("Contrast & brightness adjustment by LUT", result); // Output 이미지 출력
 	imwrite("result/result4-ii.png", result); // Output 이미지를 내 프로젝트에 별도로 저장
 
-	waitKey(); // Output 이미지들을 한 장씩 보기 위해 별도의 입력이 있을 때까지 기다린다.
+	waitKey(); // Output 이미지를 관찰하기 위해 별도의 입력이 있을 때까지 기다린다.
 
 	return 0;
 }
